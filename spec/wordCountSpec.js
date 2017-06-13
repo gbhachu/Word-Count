7e346fe4287ed3750214693d0c8846596a8dd8ec
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-06-13 11:57:29 +0100

    sorts and prints times a word appears in text

--- spec/wordCountSpec.js
+++ spec/wordCountSpec.js
@@ -1,29 +1,46 @@
-describe('wordCount', function () {
+describe('Book', function () {
   var book;
-
   beforeEach(function () {
-    book = new Book('Hello hello, how do you do? Hello to you too!');
+    book = new Book("Hello hello, how do you do. Hello to you too!");
   });
 
-  describe('initialize', function ()  {
-    it('no book exists', function () {
-      expect(book.getText()).toEqual('hello hello, how do you do? hello to you too!');
+  describe('getText', function () {
+    it('book text to lowercase', function () {
+      expect(book.getText()).toEqual("hello hello, how do you do. hello to you too!");
     });
   });
 
   describe('arrayText', function () {
-    it('inserts and separates the sample text into an array', function() {
+    it('inserts and separates the sample text into an array', function () {
       book.getText();
-      expect(book.arrayText()).toEqual(['hello', 'hello', 'how', 'do', 'you', 'do', 'hello', 'to', 'you', 'too']);
+      expect(book.arrayText()).toEqual(['hello', "hello", 'how', 'do', 'you', 'do', 'hello', 'to', 'you', 'too']);
     });
   });
 
   describe('countWords', function () {
-     it('counts and indexes words given', function () {
-       book.text = 'Hello hello, how do you do';
-       book.getText();
-       book.arrayText();
-       expect(book.countWords()).toEqual({ hello: 2, how: 1, you: 1, do: 2 });
-      });
+    it('counts word re-currence', function () {
+      book.getText();
+      book.arrayText();
+      expect(book.countWords()).toEqual({ hello: 3, how: 1, do: 2, you: 2, to: 1, too: 1 });
     });
+  });
+
+  describe('wordSorter', function () {
+    it('sorts word by times appears in text', function () {
+      book.getText();
+      book.arrayText();
+      book.countWords();
+      expect(book.wordSorter()).toEqual(['hello', 'do', "you", 'how', 'to', 'too']);
+    });
+  });
+
+  describe('printResults', function () {
+    it('prints the output with number of times word appears', function () {
+      book.getText();
+      book.arrayText();
+      book.countWords();
+      book.wordSorter();
+      expect(book.printResults()).toEqual(['hello 3', 'do 2', "you 2", 'how 1', 'to 1', 'too 1']);
+    });
+  });
 });
--- src/wordCount.js
+++ src/wordCount.js
@@ -3,6 +3,8 @@
 function Book(text) {
   this.text = text;
   this.index = [];
+  this.countWord = {};
+  this.sortWord = [];
 }
 
 Book.prototype.getText = function () {
@@ -10,21 +12,42 @@ Book.prototype.getText = function () {
 };
 
 Book.prototype.arrayText = function () {
-  this.index = this.text.match(/(\b[^\s]+\b)/g);
-  return this.index;
+  return this.text.match(/(\b[^\s]+\b)/g);
 };
 
 Book.prototype.countWords = function () {
-  var Counter = {};
+  var arrayText = this.arrayText();
 
-  for (var i = 0; i < this.index.length; i++) {
-    var word = this.index[i];
-    if (Counter.hasOwnProperty(word)) {
-      Counter[word] += 1;
+  for (var i = 0; i < arrayText.length; i++) {
+    var word = arrayText[i];
+    if (this.countWord.hasOwnProperty(word)) {
+      this.countWord[word] += 1;
     } else {
-      Counter[word] = 1;
+      this.countWord[word] = 1;
+      this.index.push(word);
     }
   }
 
-  return Counter;
+  return this.countWord;
+};
+
+Book.prototype.wordSorter = function () {
+  var countWord = this.countWord;
+
+  this.index.sort(function (a, b) {
+    var CountA = countWord[a];
+    var CountB = countWord[b];
+    return CountB - CountA;
+  });
+
+  return this.index;
+};
+
+Book.prototype.printResults = function () {
+  for (var i = 0; i < this.index.length; i++) {
+    var word = this.index[i];
+    this.sortWord.push(word + ' ' + this.countWord[word]);
+  }
+
+  return this.sortWord;
 };
