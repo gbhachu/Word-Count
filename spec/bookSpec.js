9cf91707affccd19407dd3aea3362177566a2814
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-06-13 09:11:53 +0100

    Seperate the words into an array

--- spec/bookSpec.js
+++ spec/bookSpec.js
@@ -5,8 +5,15 @@ describe('book', function () {
     book = new Book('Hello world, it is a great day to be alive!!!');
   });
 
-  it('no book exists', function () {
-    expect(book.getText()).toEqual('Hello world, it is a great day to be alive!!!');
+  describe('initialize', function ()  {
+    it('no book exists', function () {
+      expect(book.getText()).toEqual('Hello world, it is a great day to be alive!!!');
+    });
   });
 
+  describe('arrayText', function () {
+    it('inserts and separates the sample text into an array', function() {
+      expect(book.arrayText()).toEqual(['Hello', 'world', 'it', 'is', 'a', 'great', 'day', 'to', 'be', 'alive']);
+    });
+  });
 });
